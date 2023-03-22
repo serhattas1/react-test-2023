028179d015c89a6eb7ca718cbaadb0b3887458a1
serhattas1 <serhattas91@hotmail.com>
2023-03-23 00:13:11 +0300

    9.commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.RJ8
+          Edit <code>src/App.js</code> and save to reload.RJ9
         </p>
         <a
           className="App-link"
